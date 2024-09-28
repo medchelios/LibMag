06266f70c6df87f9181ccbc9deda2d6efe4822ec
MrAzuka <johntwice8@gmail.com>
2024-09-28 01:09:13 +0100

    feat: Admin Book routes

--- src/route/AdminRoute.ts
+++ src/route/AdminRoute.ts
@@ -1,11 +1,12 @@
-import { AddBookToDb } from "../controller/admin/AdminBooksController";
+import { AddBookToDb, UpdateBookInDb, DeleteBookInDb } from "../controller/admin/AdminBooksController";
 import { Router } from "express"
 import { VerifyJwtToken } from "../middleware/JwtMiddleware";
 import { IsAdminRole } from "../middleware/AdminMiddleware";
 
 const router = Router();
 
-router.post("/books",VerifyJwtToken,IsAdminRole, AddBookToDb);
-
+router.post("/books",VerifyJwtToken, IsAdminRole, AddBookToDb);
+router.put("/books/:id", VerifyJwtToken, IsAdminRole, UpdateBookInDb);
+router.delete("/books/:id", VerifyJwtToken, IsAdminRole, DeleteBookInDb);
 
 export default router
